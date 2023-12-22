cf57a4e9700405f44b5ea629db88d7bcdaca0599
joshuabutler <butlerja.nhs@gmail.com>
2023-12-21 19:13:34 -0500

    define constant choices

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/*---------------- Constants ----------------*/
+const choices = [
+  'Bite', 
+  'Scratch',
+  'Garlic',
+  'Silver Bullet',
+  'Crossbow',
+  'Wooden Stake'
+]
+
+
+/*--------------- Variables ------------------*/
+
+
+
+/*-------- Cached Element References-------*/
+
+
+
+/*-------------- Event Listeners ---------------*/
+
+
+
+/*-------------- Functions -----------------*/
